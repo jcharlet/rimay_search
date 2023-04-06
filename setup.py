baa97c2f24d9eae417447b00f6b2bce3a61dab59
Jeremie Charlet <>
2023-04-06 11:23:24 +0200

    add: cookie cutter datascience template

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='search over Rimay teachings using natural language',
+    author='Jeremie Charlet',
+    license='MIT',
+)
