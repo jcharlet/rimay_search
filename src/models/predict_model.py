2294b8586c292ea4336116d3303aac738fc45971
Jeremie Charlet <>
2023-04-11 11:16:47 +0200

    add documentation and click commands on predict_model

--- src/models/predict_model.py
+++ src/models/predict_model.py
@@ -1,6 +1,7 @@
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores import Chroma
+from langchain.docstore.document import Document
 from chromadb.config import Settings
 from langchain.chains import RetrievalQAWithSourcesChain
 from langchain import OpenAI
@@ -8,6 +9,8 @@ import pandas as pd
 from dotenv import find_dotenv, load_dotenv
 from pathlib import Path
 import logging
+from typing import Any, Dict, List, Optional
+import click
 
 load_dotenv(find_dotenv())
 
@@ -26,8 +29,12 @@ client_settings = Settings(
 logger = logging.getLogger(__name__)
 
 
+@click.group()
+def cli():
+    pass
+
 def embed_dataset_state_of_the_union(
-    input_filepath=f"{project_dir}/data/external/state_of_the_union.txt",
+    input_filepath: str =f"{project_dir}/data/external/state_of_the_union.txt",
 ) -> Chroma:
     """
     embed dataset state of the union with openai embeddings and Chroma DB
@@ -54,10 +61,22 @@ def embed_dataset_state_of_the_union(
     return collection
 
 
+@cli.command()
+@click.argument('input_filepath', type=str)
+@click.argument('collection_name', type=str)
 def embed_dataset(
-    input_filepath=f"{project_dir}/data/processed/openmindfulness_contents.csv",
-    collection_name=COL_OPENMINDFULNESS,
+    input_filepath: str =f"{project_dir}/data/processed/openmindfulness_contents.csv",
+    collection_name: str =COL_OPENMINDFULNESS,
 ) -> Chroma:
+    """embed dataset with openai embeddings and Chroma DB
+
+    Args:
+        input_filepath (str, optional): filepath of dataset to embed. Defaults to f"{project_dir}/data/processed/openmindfulness_contents.csv".
+        collection_name (str, optional): chromadb collection name. Defaults to COL_OPENMINDFULNESS.
+
+    Returns:
+        Chroma: Chroma DB collection
+    """
     logger.info(
         f"Embedding dataset with openai embeddings on collection {collection_name}"
     )
@@ -95,7 +114,16 @@ def embed_dataset(
     return collection
 
 
-def run_similarity_search(query: str, collection_name=COL_OPENMINDFULNESS):
+def run_similarity_search(query: str, collection_name: str =COL_OPENMINDFULNESS) -> list[Document]:
+    """run similarity search with openai embeddings
+
+    Args:
+        query (str): the query to run
+        collection_name (str): chromadb collection name to query
+
+    Returns:
+        List[Document]: List of documents most similar to the query text.
+    """
     logger.info(
         f"Embedding dataset with openai embeddings on collection {collection_name}"
     )
@@ -105,10 +133,24 @@ def run_similarity_search(query: str, collection_name=COL_OPENMINDFULNESS):
         client_settings=client_settings,
     )
 
-    return collection.similarity_search(query, k=4)
+    response = collection.similarity_search(query, k=4)
+    logger.info(response)
+    return response
 
 
-def run_query_with_qa_with_sources(query: str, collection_name=COL_OPENMINDFULNESS):
+@cli.command()
+@click.argument('query', type=str)
+@click.argument('collection_name', type=str)
+def run_query_with_qa_with_sources(query: str, collection_name: str = COL_OPENMINDFULNESS) -> Dict[str, Any]:
+    """ run query with openai QA
+
+    Args:
+        query (str): the question to ask
+        collection_name (str, optional): chromadb collection name to query
+
+    Returns:
+        Dict[str, Any]: {"answer": str, "sources": str}
+    """
     logger.info(f"Run query {query} on {collection_name} with openai QA")
 
     docsearch = Chroma(
@@ -125,18 +167,16 @@ def run_query_with_qa_with_sources(query: str, collection_name=COL_OPENMINDFULNE
         retriever=docsearch.as_retriever(),
         # verbose=True,
     )
-    return chain(
+    response = chain(
         {"question": query},
         return_only_outputs=True,
     )
+    logger.info(response)
+    return response
 
-
-# if __name__ == "__main__":
-#     log_fmt = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-#     logging.basicConfig(level=logging.INFO, format=log_fmt)
-
-#     # not used in this stub but often useful for finding various files
-#     project_dir = Path(__file__).resolve().parents[2]
-
-
-#     main()
+if __name__ == '__main__':
+    log_fmt = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    logging.basicConfig(level=logging.INFO, format=log_fmt)
+    logging.getLogger("chromadb").setLevel(logging.WARN)
+    logging.getLogger("clickhouse_connect").setLevel(logging.WARN)
+    cli()
